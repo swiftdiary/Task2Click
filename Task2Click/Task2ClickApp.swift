6ed58a9d4404b92cb78c5f612ee5aae9b4b34f0c
Akbar K <112651216+swiftdiary@users.noreply.github.com>
2025-12-11 16:04:32 +0500

    Initial Commit

---
+++ Task2Click/Task2ClickApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task2ClickApp.swift
+//  Task2Click
+//
+//  Created by Akbar Khusanbaev on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Task2ClickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
